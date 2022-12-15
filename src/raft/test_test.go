bb46b05ec3cf12677f5a1aa0bb6710c01ffd2d93
Vineet Ahirkar <vineet.ahirkar@oracle.com>
2022-12-14 17:42:10 -0800

    revert test changes

--- src/raft/test_test.go
+++ src/raft/test_test.go
@@ -67,35 +67,35 @@ func TestReElection2A(t *testing.T) {
 	leader1 := cfg.checkOneLeader()
 
 	// if the leader disconnects, a new one should be elected.
-	// cfg.disconnect(leader1)
-	// cfg.checkOneLeader()
+	cfg.disconnect(leader1)
+	cfg.checkOneLeader()
 
-	// time.Sleep(2 * RaftElectionTimeout)
+	time.Sleep(2 * RaftElectionTimeout)
 
-	// cfg.checkOneLeader()
+	cfg.checkOneLeader()
 
-	// // if the old leader rejoins, that shouldn't
-	// // disturb the new leader. and the old leader
-	// // should switch to follower.
-	// cfg.connect(leader1)
-	// leader2 := cfg.checkOneLeader()
+	// if the old leader rejoins, that shouldn't
+	// disturb the new leader. and the old leader
+	// should switch to follower.
+	cfg.connect(leader1)
+	leader2 := cfg.checkOneLeader()
 
 	// if there's no quorum, no new leader should
 	// be elected.
-	cfg.disconnect(leader1)
-	cfg.disconnect((leader1 + 1) % servers)
+	cfg.disconnect(leader2)
+	cfg.disconnect((leader2 + 1) % servers)
 	time.Sleep(2 * RaftElectionTimeout)
 
 	// check that the one connected server
 	// does not think it is the leader.
-	// cfg.checkNoLeader()
+	cfg.checkNoLeader()
 
 	// if a quorum arises, it should elect a leader.
-	cfg.connect((leader1 + 1) % servers)
+	cfg.connect((leader2 + 1) % servers)
 	cfg.checkOneLeader()
 
 	// re-join of last node shouldn't prevent leader from existing.
-	cfg.connect(leader1)
+	cfg.connect(leader2)
 	cfg.checkOneLeader()
 
 	cfg.end()
